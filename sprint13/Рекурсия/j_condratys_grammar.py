4adc66994f9cc963fe54b8becce4812eb34ae93a
Anton <dzanto@gmail.com>
2020-10-30 16:51:55 +0300

    Kondratiy

--- sprint13/Рекурсия/j_condratys_grammar.py
+++ sprint13/Рекурсия/j_condratys_grammar.py
@@ -1,9 +1,24 @@
+import math
+
 def solution(element):
-    row = [0, 1]
+    if element == 1:
+        return print(0)
+    elif element == 2:
+        return print(1)
+    k = int(math.sqrt(element)) + 1
+    mid = 2 ** (k-1)
+
+    if element > mid:
+        element = element - mid
+        row = [1, 0]
+    else:
+        row = [0, 1]
+
     while len(row) < element:
         second_row = [0 if i == 1 else 1 for i in row]
         row.extend(second_row)
     print(row[element-1])
+
 
 
 if __name__ == "__main__":
